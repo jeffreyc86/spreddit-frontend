ce4684d30c89a65746deffd91551f1db7aa5de93
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-10 17:58:51 -0500

    components complete

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react"
+import{Route, Switch} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
