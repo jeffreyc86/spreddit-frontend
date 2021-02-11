5f8a5eeb2e3119dfc1083e745036fe3418d571b8
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-11 16:53:29 -0500

    navbar mostly done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,6 +23,7 @@ function Navbar ({channels}) {
     return (
         <div className="navbar">
             <img className="navbar-logo" src={process.env.PUBLIC_URL + "/images/logo.jpg"} alt="spreddit" onClick={null}/>
+            {/* only shows when currentuser is set */}
             <div className="dropdown">
                 <button className="dropbtn">Select a Channel</button>
                 <div className="dropdown-content">
@@ -30,8 +31,12 @@ function Navbar ({channels}) {
                 </div>
             </div>
             <div className="navbar-right">
+                   {/* only shows when currentuser is null */}
                 <NavLink to="/signup" exact>Sign Up</NavLink>
                 <NavLink to="/login" exact>Login</NavLink>
+                   {/* only shows when currentuser is set */}
+                <NavLink to="/user" exact>Profile</NavLink>
+                <NavLink to="/logout" exact>Logout</NavLink>
             </div>
         </div>
     )
