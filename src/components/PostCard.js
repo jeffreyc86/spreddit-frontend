26c1b657057b4e48188e6d69850df1bfbb34044f
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-14 22:04:55 -0500

    navbar complete

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,4 +1,20 @@
 import React from 'react'
 
+function PostCard({post}){
+
+
+
+    return (
+        <div className="post-card">
+            <h2 className="post-title">{post.title}</h2>
+            <h6>{post.anonymous ? "Anonymous" : post.author}</h6>
+            <p className="post-content">{post.content}</p>
+            <img className="post-image" src={post.image_url} alt={post.title} />
+            <p className="post-likes-count">Likes: {post.likes.length}</p>
+            <p className="post-comments-count">{post.comments.length} Comments</p>
+        </div>
+    )
+}
+
 
 export default PostCard;
--- src/components/channel-container/ChannelContainer.js
+++ src/components/channel-container/ChannelContainer.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
 import ChannelDetails from "./ChannelDetails"
 import Search from "./Search"
+import PostCard from "../PostCard"
 
 function ChannelContainer({currentUser}){
 
@@ -10,14 +11,23 @@ function ChannelContainer({currentUser}){
 
     const [channel, setChannel] = useState(null)
     const [search, setSearch] = useState("")
+    const [posts, setPosts] = useState([])
 
     useEffect(()=>{
         fetch(`${API}channels/${id}`)
             .then(res=>res.json())
             .then(channelObj => {
                 setChannel(channelObj)
+                setPosts(channelObj.posts)
             })
     },[id])
+
+    const filteredPosts = posts.filter(post => {
+        return post.title.toLowerCase().includes(search.toLowerCase())
+    })
+    .map(post => {
+        return <PostCard key={post.id} post={post} />
+    })
 
 
     return(
@@ -28,7 +38,7 @@ function ChannelContainer({currentUser}){
                 <Search search={search} setSearch={setSearch} />
             </>
             : <h1>Loading...</h1>}
-           
+            {filteredPosts}
         </div>
     )
 }
