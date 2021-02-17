56bcb8f59fbd5eb1c841cc7e713faf21ff8ce062
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-17 14:48:34 -0500

    95% done

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,16 +29,23 @@ function Home({currentUser}){
             <div className="home-description">
                 <div className="home-banner">
                     <img src={process.env.PUBLIC_URL + "/images/homebanner.png"} alt="home"></img>
-                    <div className="home-description">
+                    <div className="home-quote">
                         <h1>A Global Community of</h1>
                         <h1>Like Minded Humans</h1>
                         <h1>Socially Distancing</h1>
                     </div>
                 </div>
-                <p>{quote}</p>
-                <p> - {quoteAuthor}</p>
             </div>
-            {currentUser ? null : <div className="home-signup"><h1 onClick={handleSignUpClick}>Sign Up to View More!</h1></div>}
+                <div className="random-quote">
+                    <h4>Motivational Quote To Get Through The Day</h4>
+                        <div className="quote-div">
+                            <img src={process.env.PUBLIC_URL + "/images/openquote.jpg"} alt="open quote"></img>
+                            <p className="quote">{quote}</p>
+                            <img src={process.env.PUBLIC_URL + "/images/closequote.jpg"} alt="close quote"></img>
+                        </div>
+                    <p className="quote-author">{quoteAuthor}</p>
+                </div>
+            {currentUser ? null : <div className="home-signup"><h1 onClick={handleSignUpClick}>Join Our Community!</h1></div>}
         </div>
     )
 }
