d33397d9c6b159d2becf8c9d9080a6af83f5f696
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-17 23:32:23 -0500

    footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ function Footer(){
 
     return (
         <div className="footer">
-            <a href="https://www.cdc.gov/coronavirus/2019-ncov/daily-life-coping/managing-stress-anxiety.html">Mental Health Support</a>
+            <a id="footer-mh" href="https://www.cdc.gov/coronavirus/2019-ncov/daily-life-coping/managing-stress-anxiety.html">Mental Health Support</a>
             <img className="footer-logo" src={process.env.PUBLIC_URL + "/images/spreddit.png"} alt="🙂" onClick={logoClick}/>
             <a href="https://www.cdc.gov/coronavirus/2019-ncov/index.html">CDC Covid-19 Guidlines</a>
         </div>
